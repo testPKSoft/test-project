d0443e54c411c31fb04e967255af4c6e1ff18e14
Azamat Orozbaev <44497623+songhee24@users.noreply.github.com>
2021-03-17 18:49:45 +0600

    method print deleted

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,8 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("hello");
     }
-    public static void print(){
-        System.out.println("hello");
-    }
+
 }
